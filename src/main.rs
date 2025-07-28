7087e6b131f8ced27c35a7d9c4e2b8148d8769b6
MeexReay <meexreay@gmail.com>
2025-07-28 15:09:55 +0300

    write main rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,34 @@
+use clap::Parser;
+
 mod client;
 mod server;
 
-fn main() {
-    println!("Hello, world!");
+/// Proxy that based on QUIC
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Connect to proxy
+    #[arg(short, long)]
+    connect: Option<String>,
+
+    /// Bind proxy
+    #[arg(short, long)]
+    bind: Option<String>,
+    
+    /// Password
+    #[arg(short, long, default_value = "nope")]
+    password: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+
+    if let Some(host) = args.bind {
+        todo!()
+    } else if let Some(host) = args.connect {
+        todo!()
+    } else {
+        println!("choose either --connect or --bind")
+    }
 }
